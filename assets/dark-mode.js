34be8ff6d5971801644dde5ba87ed3e701312432
52db <clwpost@gmail.com>
2025-08-02 11:25:46 +0800

    Create dark-mode.js

---
+++ assets/dark-mode.js
@@ -0,0 +1,7 @@
+// assets/dark-mode.js
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+  body.classList.toggle('dark-mode');
+});
